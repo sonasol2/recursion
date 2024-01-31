887d9d6f0b56bea65d30643b47c57c6c304e0841
sonasol2 <57984322+sonasol2@users.noreply.github.com>
2024-01-31 17:48:28 +0200

    done

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -4,7 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int Factorial(int value)
+        {
+            if (value == 1) return 1;
+
+            return value * Factorial(value - 1);
+	    }
+
+        Factorial(5);
+
+        int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+
+        int fib4 = Fibonachi(4);
+        int fib5 = Fibonachi(5);
+        int fib6 = Fibonachi(6);
+
+        Console.WriteLine($"4 число Фибоначчи = {fib4}");
+        Console.WriteLine($"5 число Фибоначчи = {fib5}");
+        Console.WriteLine($"6 число Фибоначчи = {fib6}");
+
     }
+
 }
 
